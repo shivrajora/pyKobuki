0ca16c39e9af6875e0218e4fbd054e7c6f284346
shivrajora <rajora.shiv@gmail.com>
2017-06-06 15:05:08 -0400

    script fixed

--- make_video.sh
+++ make_video.sh
@@ -1,3 +1,10 @@
-mkdir tmp
-x=1; for i in *depth.png; do counter=$(printf %03d $x); ln "$i" tmp/img"$counter".png; x=$(($x+1)); done
+rm -r data/tmp
+mkdir data/tmp
+x=1; for i in data/*depth.png; do counter=$(printf %03d $x); ln "$i" data/tmp/depth_img"$counter".png; x=$(($x+1)); done
+x=1; for i in data/*rgb.png; do counter=$(printf %03d $x); ln "$i" data/tmp/rgb_img"$counter".png; x=$(($x+1)); done
 
+./ffmpeg -i data/tmp/depth_img%03d.png -r 10 -vcodec mpeg4 data/tmp/depth.avi
+
+./ffmpeg -i data/tmp/rgb_img%03d.png -r 10 -vcodec mpeg4 data/tmp/rgb.avi
+
+rm data/tmp/*.png
