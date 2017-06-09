8fe3412b27345ca64703b7845493271cdcfd98d9
shivrajora <rajora.shiv@gmail.com>
2017-06-09 14:45:08 -0400

    changes

--- make_video.sh
+++ make_video.sh
@@ -1,10 +1,12 @@
-rm -r data/tmp
-mkdir data/tmp
-x=1; for i in data/*depth.jpg; do counter=$(printf %03d $x); ln "$i" data/tmp/depth_img"$counter".jpg; x=$(($x+1)); done
-x=1; for i in data/*rgb.jpg; do counter=$(printf %03d $x); ln "$i" data/tmp/rgb_img"$counter".jpg; x=$(($x+1)); done
+D1=$1
+echo $D1"tmp"
+rm -r $D1"tmp"
+mkdir $D1"tmp"
+x=1; for i in $D1*depth.jpg; do counter=$(printf %03d $x); ln "$i" $D1"tmp/depth_img""$counter".jpg; x=$(($x+1)); done
+x=1; for i in $D1*rgb.jpg; do counter=$(printf %03d $x); ln "$i" $D1"tmp/rgb_img""$counter".jpg; x=$(($x+1)); done
 
-./ffmpeg -i data/tmp/depth_img%03d.jpg -r 10 -vcodec mpeg4 data/tmp/depth.avi
+./ffmpeg -i $D1"tmp"/depth_img%03d.jpg -r 10 -vcodec mpeg4 $D1"tmp/depth.avi"
 
-./ffmpeg -i data/tmp/rgb_img%03d.jpg -r 10 -vcodec mpeg4 data/tmp/rgb.avi
+./ffmpeg -i $D1"tmp"/rgb_img%03d.jpg -r 10 -vcodec mpeg4 $D1"tmp/rgb.avi"
 
-rm data/tmp/*.jpg
+rm $D1"tmp"/*.jpg
